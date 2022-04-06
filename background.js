d0e6d4ebf5ca7c9849d008a604b159d1406aeb0a
Robert <oliveira.rlde@gmail.com>
2022-04-06 00:52:44 -0400

    Trim copyright notice

--- background.js
+++ background.js
@@ -14,6 +14,4 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-    You can contact the author by email at robolivable@gmail.com.
 */
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -14,8 +14,6 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-    You can contact the author by email at robolivable@gmail.com.
 */
 import World from './world'
 import Classes from './classes'
