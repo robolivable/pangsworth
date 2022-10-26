6dce4346c955f6c02553ef30833c0e364d094b6d
Robert <oliveira.rlde@gmail.com>
2022-10-26 19:21:48 -0400

    Add release process documentation

--- scripts/create-packable.sh
+++ scripts/create-packable.sh
@@ -6,4 +6,13 @@ RELEASE_PATH=${PWD}/releases/${NAME}-${VERSION}
 
 npm run build-release \
   && rm -rf $RELEASE_PATH && mkdir -p $RELEASE_PATH \
-  && cp -R ${PWD}/manifest.json ${PWD}/build ${PWD}/static $RELEASE_PATH
+  && cp -R ${PWD}/manifest.json ${PWD}/build ${PWD}/static $RELEASE_PATH \
+  && echo "#############################################################" \
+  && echo "-" \
+  && echo "Release prepared:"\
+  && echo "${RELEASE_PATH}" \
+  && echo "-" \
+  && echo "Please use the browser's extension packing tool to pack this" \
+  && echo "release. Then run \`npm run prepare-release\` to generate the" \
+  && echo "shasum file before uploading." \
+  && echo "#############################################################" \
